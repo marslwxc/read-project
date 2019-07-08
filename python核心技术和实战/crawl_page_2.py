70770e20f48513ab1ac69ec5205dc1f4c7efd278
mars <770753243@qq.com>
2019-07-08 23:28:52 +0800

    update

--- python核心技术和实战/crawl_page_2.py
+++ python核心技术和实战/crawl_page_2.py
@@ -6,11 +6,11 @@ async def crawl_page_2(url):
     sleep_time = int(url.split('_')[-1])
     await asyncio.sleep(sleep_time)
     print('OK {}'.format(url))
-    
-    
+
+
 async def main_2(urls):
     tasks = [asyncio.create_task(crawl_page_2(url)) for url in urls]
     for task in tasks:
         await task
-        
+
 asyncio.run(main_2(['url_1', 'url_2', 'url_3', 'url_4']))
