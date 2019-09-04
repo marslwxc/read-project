be721a4f6ea557d1e67997072237b5e2bcc20930
mars <770753243@qq.com>
2019-09-04 18:27:31 +0800

    1

--- 数据结构与算法之美/数组 链表/single_linked_list.py
+++ 数据结构与算法之美/数组 链表/single_linked_list.py
@@ -1,3 +1,8 @@
+"""
+单链表
+插入 删除 查找操作
+链表中存储的数据类型是Int
+"""
 class Node:
     def __init__(self, data, next_node):
         self.data = data
@@ -7,3 +12,16 @@ class Node:
 class SingleLinkedList:
     def __init__(self):
         self._head = None
+
+    def find_by_value(self, data):
+        p = self._head
+        while p and p != data:
+            p = p._next
+        return p
+
+    def find_by_index(self, index):
+        p = self._head
+        position = 0
+        while p and index != position:
+            p = p._next
+        return p
