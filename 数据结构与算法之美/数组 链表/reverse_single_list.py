8c45af3e40fe654530a6d82af66cb3b946afe74c
HanzheZhang <770753243@qq.com>
2019-09-06 15:19:23 +0800

    link_list

---
+++ 数据结构与算法之美/数组 链表/reverse_single_list.py
@@ -0,0 +1,27 @@
+"""
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur = head
+        prev = None
+        while cur != None:
+            n = cur.next
+            cur.next = prev
+            prev = cur
+            cur = n
+        return prev
