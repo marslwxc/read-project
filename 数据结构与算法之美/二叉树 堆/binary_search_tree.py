d28220de25866e724bc290c7d126678f8ef806e8
HanzheZhang <770753243@qq.com>
2019-09-12 11:01:46 +0800

    tree

---
+++ 数据结构与算法之美/二叉树 堆/binary_search_tree.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+
+class BirnaySearchNode:
+    def __init__(self):
+        self._root = None
